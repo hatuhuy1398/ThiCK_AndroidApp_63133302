1aab6258ec5d8dda866157b38c90c2e9a8d7df8e
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-05-28 17:14:25 +0700

    detail shop app ui

---
+++ app/src/main/java/hatuhuy1398/thick_androidapp_63133302/Activity/DetailActivity.java
@@ -0,0 +1,23 @@
+package hatuhuy1398.thick_androidapp_63133302.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import hatuhuy1398.thick_androidapp_63133302.R;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail);
+
+
+    }
+}
